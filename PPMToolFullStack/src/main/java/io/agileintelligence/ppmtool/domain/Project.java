b880a327cca3ed69c495f0f71fb44431332613b4
Steve Read <steve.read@ageas.co.uk>
2023-03-27 15:32:19 +0100

    Set up Object Validation

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,6 +1,11 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import javax.*;
 
 import java.util.Date;
 
@@ -10,13 +15,30 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Project name is required")
     private String projectName;
+
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min=4, max=5, message="Please use 4 to 5 characters")
+    @Column(updatable=false, unique=true)
     private String projectIdentifier;
+
+    @NotBlank(message = "Description is required")
     private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
+
 
     public Project() {
     }
